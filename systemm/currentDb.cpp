168ee7874654797cf12913539325f4c765b57253
ZhouJianyu <zjybox@126.com>
2015-12-19 02:57:22 +0800

    create dbmanager

--- systemm/currentDb.cpp
+++ systemm/currentDb.cpp
@@ -4,13 +4,13 @@
 #include <vector>
 #define MAX_DB_AMOUNT
 using namespace std;
-class DbManager{					//单例，全局唯一 
+class DbManager{							
 	public:
-		vector<string> dbNames; 	//维护所有数据库的名字 
-		int createDb(string name);	//创建一个数据库，成功返回1，失败返回0 
+		vector<string> dbNames; 			//维护所有数据库的名字 
+		int createDb(string name);			//创建一个数据库，成功返回1，失败返回0 
 		const string showCurDbName();		//返回当前数据库的名称
 		const string showCurDbPath();		//返回当前数据库的绝对路径 
-		static DbManager* getInstance();
+		static DbManager* getInstance();	//单例，全局唯一 
 	private:
 		DbManager(){}
 		DbManager(const DbManager&){} 
@@ -22,11 +22,16 @@ class DbManager{					//单例，全局唯一
 };
 
 DbManager* DbManager::manager = new DbManager();
+
 int DbManager::createDb(string name){
 	int result = DbCreator::createFolder(name);	
-	if(result==1){curDbName = DbCreator::dbName; curDbPath = DbCreator::dbPath;}
+	if(result==1){	//新目录创建成功 
+		curDbName = DbCreator::dbName; curDbPath = DbCreator::dbPath;
+		dbNames.push_back(curDbName);
+	}
 	return result;
 }
+
 DbManager* DbManager::getInstance(){
 	if(manager==NULL){
 		manager = new DbManager();
